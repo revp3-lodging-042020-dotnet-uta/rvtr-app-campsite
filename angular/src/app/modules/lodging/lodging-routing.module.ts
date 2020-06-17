33fdecbc6070850e00e439195b776ee3992b3f90
Jayson Lennon <jayson@jaysonlennon.dev>
2020-06-17 13:37:57 -0700

    save

--- angular/src/app/modules/lodging/lodging-routing.module.ts
+++ angular/src/app/modules/lodging/lodging-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LodgingComponent } from './lodging/lodging.component';
+import { LodgingSearchFormComponent } from './lodging-search-form/lodging-search-form.component';
 
-const routes: Routes = [{ component: LodgingComponent, path: '' }];
+const routes: Routes = [{ component: LodgingComponent, path: '' },
+                        { component: LodgingSearchFormComponent, path: 'search'}];
 
 @NgModule({
   declarations: [],
--- angular/src/app/modules/lodging/lodging-search-form/lodging-search-form.component.ts
+++ angular/src/app/modules/lodging/lodging-search-form/lodging-search-form.component.ts
@@ -1,11 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { Lodging } from 'src/app/data/lodging.model';
+import { LodgingSortKey } from '../@types/lodging-sort-key';
+import { SortOrder } from '../@types/sort-order';
 
 @Component({
-  selector: 'uic-lodging',
+  selector: 'uic-lodging-search-form',
   templateUrl: './lodging-search-form.component.html',
 })
 export class LodgingSearchFormComponent implements OnInit {
+
+  private sortByFields: {name: string, value: LodgingSortKey }[] = [
+    { name: 'Average rating', value: LodgingSortKey.ReviewAverageRating },
+    { name: 'Number of reviews', value: LodgingSortKey.ReviewCount },
+    { name: 'Number of beds', value: LodgingSortKey.Beds },
+    { name: 'Number of baths', value: LodgingSortKey.Bathrooms },
+    { name: 'Max occupancy', value: LodgingSortKey.Occupancy },
+    { name: 'Name', value: LodgingSortKey.Name },
+  ];
+
+  private sortOrderFields: {name: string, value: SortOrder }[] = [
+    { name: 'Ascending', value: SortOrder.Ascending },
+    { name: 'Descending', value: SortOrder.Descending },
+  ];
 
   constructor() {}
 
