a628e2b48436f0b16062a150d070658a1ca5f385
Will R <will.ruizdecastilla@gmail.com>
2020-06-17 14:54:45 -0500

    state at current ...error..not pulling data

--- angular/src/app/modules/lodging/lodging-display-review/lodging-display-review.component.ts
+++ angular/src/app/modules/lodging/lodging-display-review/lodging-display-review.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
+import { LodgingService } from './../../../services/lodging/lodging.service';
+import { Lodging } from 'src/app/data/lodging.model';
+import { HttpParams } from '@angular/common/http';
+import { ReviewService } from './../../../services/lodging/review.service';
+import { ReviewQueryParams } from '../@types/review-query-params';
+import { Review } from 'src/app/data/review.model';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [CommonModule]
+})
 
 @Component({
   selector: 'uic-lodging-display-review',
@@ -7,9 +18,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LodgingDisplayReviewComponent implements OnInit {
 
-  constructor() { }
+  constructor(private reviewService: ReviewService) { }
 
   ngOnInit(): void {
+
+    this.queryParamsReview();
   }
+  private reviews: Review[];
+
+  queryParamsReview(): void {
+    let params = new HttpParams();
+    params = params.set(ReviewQueryParams.Limit, '5');
+    params = params.set(ReviewQueryParams.Offset, '0');
+
+
+    // use undefined to skip the optional 'id' parameter
+    this.reviewService.get(undefined, params).subscribe(response => {
+      this.reviews = response;
+    });
+  }
+  //review get change the offset amount based on how many have loaded
+  LoadMore(change: number): void {  //
+
+    let params = new HttpParams();
+    params = params.set(ReviewQueryParams.Limit, '5');
+    //+ unary used instead of parseInt
+    change += + params.get(ReviewQueryParams.Limit)
+
+    params = params.set(ReviewQueryParams.Offset, change.toString());
+
+    this.reviewService.get(undefined, params).subscribe(response => {
+      this.reviews = response;
+    });
+  }
+
 
 }
--- angular/src/app/modules/lodging/lodging-routing.module.ts
+++ angular/src/app/modules/lodging/lodging-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LodgingComponent } from './lodging/lodging.component';
+import { LodgingDisplayReviewComponent } from './lodging-display-review/lodging-display-review.component';
 
-const routes: Routes = [{ component: LodgingComponent, path: '' }];
+const routes: Routes = [{ component: LodgingComponent, path: '' },
+{ component: LodgingDisplayReviewComponent, path: 'review' }
+];
 
 @NgModule({
   declarations: [],
   exports: [RouterModule],
   imports: [RouterModule.forChild(routes)],
 })
-export class LodgingRoutingModule {}
+export class LodgingRoutingModule { }
