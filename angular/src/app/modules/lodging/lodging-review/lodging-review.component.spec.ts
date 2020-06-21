f1cc3c03764df4854018e289eb50f6d46632561d
Jayson Lennon <jayson@jaysonlennon.dev>
2020-06-21 10:32:29 -0700

    Fix review tests

--- angular/src/app/modules/lodging/lodging-review/lodging-review.component.spec.ts
+++ angular/src/app/modules/lodging/lodging-review/lodging-review.component.spec.ts
@@ -1,14 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LodgingReviewComponent } from './lodging-review.component';
+import {
+  HttpClientTestingModule,
+} from '@angular/common/http/testing';
+import { AccountService } from 'src/app/services/account/account.service';
 
 describe('LodgingReviewComponent', () => {
   let component: LodgingReviewComponent;
   let fixture: ComponentFixture<LodgingReviewComponent>;
+  let accountService: AccountService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LodgingReviewComponent ]
+      declarations: [ LodgingReviewComponent ],
+      imports: [HttpClientTestingModule],
+      providers: [{ provide: AccountService, useValue: accountService }],
     })
     .compileComponents();
   }));
